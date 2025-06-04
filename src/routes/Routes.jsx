07bd909796779eb6247571597a76649944564715
desarrollo <desarrollo@academicos.cesde.edu.co>
2025-06-04 08:52:19 -0500

    feat se corrige erorr de ortografia

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Login from '../pages/Login';
 import Register from '../pages/Register';
-import Dashboard from '../pages/Dashboard';
+import Dashboard from '../pages/DashBoard';
 import { useAuth } from '../context/AuthContext';
 
 const RoutesApp = () => {
